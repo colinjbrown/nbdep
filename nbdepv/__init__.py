7546575396d240cd000610117469b0dc35c70aeb
Colin Brown <cbrown12@umassd.edu>
2018-11-14 12:09:16 -0500

    Include all levels of a package
    I've commented out the logic that finds the top levels for now in the case that we'd like to support only top levels in the future.

--- nbdepv/__init__.py
+++ nbdepv/__init__.py
@@ -21,32 +21,46 @@ class ImportWatcher(object):
         self.comm = Comm(target_name='nbdepv', data={})
         self.sys = sys
         self.shell = ip
-        self.modules = self.get_top_levels(list(sys.modules))
+        self.modules = list(sys.modules) #self.get_top_levels(list(sys.modules))
         self.logger = logging.getLogger()
         self.debug = False
 
-    @staticmethod
-    def get_top_levels(modules):
-        top_levels = set()
-        for mod in modules:
-            top_levels.add(mod.split('.')[0])
-        return top_levels
+    #No longer rely on this method since we export more than just top levels now
+    # @staticmethod
+    # def get_top_levels(modules):
+    #     top_levels = set()
+    #     for mod in modules:
+    #         top_levels.add(mod.split('.')[0])
+    #     return top_levels
 
     def grab_modules(self):
-        new_mods = self.get_top_levels(list(self.sys.modules))
+        new_mods = list(self.sys.modules) #self.get_top_levels(list(self.sys.modules))
         incoming = set(new_mods) - set(self.modules)
         self.modules = new_mods
         metadata = {}
         for mod in incoming:
             try:
-                imported = __import__(mod)
+                imported = self.sys.modules[mod]
+                #imported = __import__(mod)
                 try:
                     v = imported.__version__
                 #Does not have a valid version number
                 except AttributeError:
                     v = "Unknown"
-            #Not able to be __import__'d
-            except ModuleNotFoundError:
+                    if '.' in mod:
+                        top_level = mod.rsplit('.',1)[0]
+                        if top_level in metadata:
+                            v = metadata[top_level]
+                        else:
+                            try:
+                                top_level = mod.split('.')[0]
+                                v = self.sys.modules[top_level].__version__
+                            #Triggers both when the top level isn't imported
+                            #and if __version__ isn't valid on the top level library
+                            except AttributeError:
+                                pass
+            #Unknown key, shouldn't happen, but we'd rather catch this
+            except AttributeError:
                 v = "Unknown"
             metadata[mod] = v
             if self.debug:
